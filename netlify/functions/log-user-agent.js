a232aa5dfc653407efc53b747ac8edb49b82502c
Loren Stephens <ldstep@ldstephens.com>
2025-06-13 15:43:44 -0400

    Delete log-user-agent.js

--- netlify/functions/log-user-agent.js
+++
@@ -1,9 +0,0 @@
-exports.handler = async (event, context) => {
-   const userAgent = event.headers["user-agent"] || "No UA";
-   console.log("User-Agent:", userAgent);
-
-   return {
-      statusCode: 200,
-      body: "Thanks for visiting!",
-   };
-};
